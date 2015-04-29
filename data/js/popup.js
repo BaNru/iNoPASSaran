2d0b3c362972350b43a21286a8226e733ad323b4
BaNru <admin@g63.ru>
2015-04-29 06:36:33 +0400

    Added new extended Algorithm; Added aliases for the basic Algorithm

--- data/js/popup.js
+++ data/js/popup.js
@@ -36,8 +36,12 @@ if (typeof chrome !== "undefined"){
  *
  */
 function genPass(a,pass,salt,url){
-	var strAlg = '',
-		l = a.length;
+	var strAlg = '', l;
+	a = a.toLowerCase();
+	if (a.indexOf(' ') >= 0) {
+		a = a.split(' ');
+	}
+	l = a.length;
 	for(var i=0; i<l; i++){
 		strAlg += alg(
 			a[i],
@@ -62,19 +66,39 @@ function genPass(a,pass,salt,url){
  * 
  * a - generate algorithm
  * pass - Master Password
- * salt - salt from plugin chrome localStorage
+ * salt - Salt
  * url - URL
  *
- * 0 - Доменое имя целиком
- * 1 - Доменая зона целиком
- * 2 - Первая половина домена
- * 3 - Вторая половина домена
- * 4 - Количества символов в домене
- * 5 - Количества символов в зоне
- * 6 - Соль целиком
- * 7 - Мастер пароль целиком
- * 8 - Первая половина мастер пароля
- * 9 - Вторая половина мастер пароля
+ * Basic:
+ *
+ * 0 - Доменое имя целиком					alias: a, k, u, а, й, у, э
+ * 1 - Доменая зона целиком					alias: b, l, v, б, к, ф, ю
+ * 2 - Первая половина домена				alias: c, m, w, в, л, х, я
+ * 3 - Вторая половина домена				alias: d, n, x, г, м, ц, -
+ * 4 - Количества символов в домене			alias: e, o, y, д, н, ч, -
+ * 5 - Количества символов в зоне			alias: f, p, z, е, о, ш, -
+ * 6 - Соль целиком							alias: g, q, -, ё, п, щ, -
+ * 7 - Мастер пароль целиком				alias: h, r, -, ж, р, ъ, -
+ * 8 - Первая половина мастер пароля		alias: i, s, -, з, с, ы, -
+ * 9 - Вторая половина мастер пароля		alias: j, t, -, и, т, ь, -
+ *
+ * Extended:
+ *
+ * 10 - Перевернутое доменое имя целиком
+ * 11 - Перевернутая доменая зона целиком
+ * 12 - Перевернутая первая половина домена
+ * 13 - Перевернутая вторая половина домена
+ * 14 - нет
+ * 15 - нет
+ * 16 - Перевернутая соль целиком
+ * 17 - Перевернутый мастер пароль целиком
+ * 18 - Перевернутая первая половина мастер пароля
+ * 19 - Перевернутая вторая половина мастер пароля
+ *
+ * 160 - Первая половина соли
+ * 161 - Вторая половина соли
+ * 162 - Перевернутая первая половина соли
+ * 163 - Перевернутая вторая половина соли
  *
 */
 function alg(a,pass,salt,url) {
@@ -99,40 +123,92 @@ function alg(a,pass,salt,url) {
 //		_8		= _8_9[0],
 //		_9		= _8_9[1];
 
-	if (a == 0) {
+// Говнокод mode ON :D
+
+	// Basic
+
+	if (['0', 'a', 'k', 'u', 'а', 'й', 'у', 'э'].indexOf(a) >= 0){
 		return new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1];
 	};
-	if (a == 1) {
+	if (['1', 'b', 'l', 'v', 'б', 'к', 'ф', 'ю'].indexOf(a) >= 0){
 		return new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[2];
 	};
-	if (a == 2) {
+	if (['2', 'c', 'm', 'w', 'в', 'л', 'х', 'я'].indexOf(a) >= 0){
 		return halfString(
 			new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1]
 		)[0]
 	};
-	if (a == 3) {
+	if (['3', 'd', 'n', 'x', 'г', 'м', 'ц'].indexOf(a) >= 0){
 		return halfString(
 			new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1]
 		)[1]
 	};
-	if (a == 4) {
+	if (['4', 'e', 'o', 'y', 'д', 'н', 'ч'].indexOf(a) >= 0){
 		return new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1].length;
 	};
-	if (a == 5) {
+	if (['5', 'f', 'p', 'z', 'е', 'о', 'ш'].indexOf(a) >= 0){
 		return new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[2].length;
 	};
-	if (a == 6) {
+	if (['6', 'g', 'q', 'ё', 'п', 'щ'].indexOf(a) >= 0){
 		return salt;
 	};
-	if (a == 7) {
+	if (['7', 'h', 'r', 'ж', 'р', 'ъ'].indexOf(a) >= 0){
 		return pass;
 	};
-	if (a == 8) {
+	if (['8', 'i', 's', 'з', 'с', 'ы'].indexOf(a) >= 0){
 		return halfString(pass)[0];
 	};
-	if (a == 9) {
+	if (['9', 'j', 't', 'и', 'т', 'ь'].indexOf(a) >= 0){
 		return halfString(pass)[1];
 	};
+
+	// Extended
+	if (a == 10) {
+		return reverseString(new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1]);
+	}
+	if (a == 11) {
+		return reverseString(new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[2]);
+	}
+	if (a == 12) {
+		return reverseString(halfString(
+			new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1]
+		)[0])
+	}
+	if (a == 13) {
+		return reverseString(halfString(
+			new URL(url).hostname.replace('www.','').match(/(.*)\.(.*$)/)[1]
+		)[1])
+	}
+	if (a == 14) {}
+	if (a == 15) {}
+	if (a == 16) {
+		return reverseString(salt);
+	}
+	if (a == 17) {
+		return reverseString(pass);
+	}
+	if (a == 18) {
+		return reverseString(halfString(pass)[0]);
+	}
+	if (a == 19) {
+		return reverseString(halfString(pass)[1]);
+	}
+
+	if (a == 160) {
+		return halfString(salt)[0];
+	}
+	if (a == 161) {
+		return halfString(salt)[1];
+	}
+	if (a == 162) {
+		return reverseString(halfString(salt)[0]);
+	}
+	if (a == 163) {
+		return reverseString(halfString(salt)[1]);
+	}
+
+// Говнокод mode OFF :D
+
 	return '';
 }
 
@@ -157,4 +233,16 @@ function halfString(str){
 		t = f + 1;
 	}
 	return [ str.substring(0,t), str.substring(t,l) ];
+}
+
+/**
+ * Reverse String
+ *
+ * Переворачивает строху (str) задом наперед.
+ *
+ * Return string
+ *
+ */
+function reverseString(str) {
+	return str.split("").reverse().join("");
 }
